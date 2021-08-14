e5633a535952faedb82443db6c26f0f5b25fcf6e
unknown <a1002android@gmail.com>
2021-08-14 22:14:21 +0300

    feat: done, but add check two-digit number

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable (number) {
-    const collation_dict = {
+    const collation_list = {
         0: 'zero',
         1: 'one',
         2: 'two',
@@ -29,11 +29,26 @@ module.exports = function toReadable (number) {
         80: 'eighty',
         90: 'ninety',
     }
-    let result;
-    let convert_number = number.toString();
-    if (Object.keys(collation_dict).includes(convert_number)) {
-        return result = collation_dict[convert_number];
+    let result, first_digit, second_digit, last_digit;
+    let n = 0, first_str = '', second_str = '', last_str = ''; 
+    let converted_number = number.toString();
+    if (Object.keys(collation_list).includes(converted_number)) {
+        // return result = collation_list[converted_number];
+        return collation_list[converted_number];
     }
-
-
+    if (converted_number.length == 3) {
+        first_digit = converted_number[0];
+        first_str = collation_list[first_digit] + ' hundred';
+        n = 1;
+    }
+    if (converted_number[n] != 0) {
+        second_digit = converted_number[n] + '0';
+        second_str = ' ' + collation_list[second_digit];
+    }
+    if (converted_number[n + 1] != 0) {
+        last_digit = converted_number[n + 1];
+        last_str = ' ' + collation_list[last_digit];
+    }
+    
+    return first_str + second_str + last_str;
 }
